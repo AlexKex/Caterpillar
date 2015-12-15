5f730ad491cd32d9e74694cc36d1594569bfc3a6
apryakhin <8s7sKodA0646>
2015-12-15 14:12:53 +0300

    WeatherWidget V.0.3.1
    fixes:
    useless variables removed
    added forecast item css
    
    refactoring:
    ForecastItem assembling logic moved to static method

--- src/main/java/module/service/Weather/WeatherForecastItem.java
+++ src/main/java/module/service/Weather/WeatherForecastItem.java
@@ -1,15 +1,41 @@
 package module.service.Weather;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+
+import java.io.IOException;
+
 /**
  * Created by apryakhin on 09.12.2015.
  */
 public class WeatherForecastItem {
     protected String item_time;
-    protected int item_timestamp;
     protected String item_label;
     protected double temperature_celsius;
     protected double temperature_fahrenheit;
     protected String icon_ref;
+
+    public static GridPane getForecastPane(FXMLLoader item_loader, WeatherForecastItem forecast_data) throws IOException {
+        GridPane item_pane = item_loader.load();
+
+        Label time_label = new Label(forecast_data.getItemLabel());
+        item_pane.add(time_label, 0, 0);
+
+        Double celsius = forecast_data.getTemperatureCelsius();
+        Double fahrenheit = forecast_data.getTemperatureFahrenheit();
+        Label weather_label = new Label(
+                celsius.intValue() + " C | " + fahrenheit.intValue() + " F"
+        );
+        item_pane.add(weather_label, 0, 1);
+
+        ImageView icon = new ImageView(new Image(forecast_data.getIconRef()));
+        item_pane.add(icon, 0, 3);
+
+        return item_pane;
+    }
 
     public String getItemTime(){
         return this.item_time;
